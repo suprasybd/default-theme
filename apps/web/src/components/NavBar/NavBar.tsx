208e6b10b84d21fe2096dc57c7d2550951ba7664
Alvee <M.Alvee8141@gmail.com>
2024-03-25 21:32:19 +0600

    Update NavBar.tsx

--- apps/web/src/components/NavBar/NavBar.tsx
+++ apps/web/src/components/NavBar/NavBar.tsx
@@ -139,23 +139,6 @@ const NavBar: React.FC = () => {
           </nav>
         </section>
       </div>
-
-      <div className="absolute bottom-0 right-0 mb-4 mr-4 z-10">
-        <div>
-          <a
-            title="Follow me on twitter"
-            href="https://www.twitter.com/asad_codes"
-            target="#"
-            className="block w-16 h-16 rounded-full transition-all shadow hover:shadow-lg transform hover:scale-110 hover:rotate-12"
-          >
-            <img
-              alt="something"
-              className="object-cover object-center w-full h-full rounded-full"
-              src="https://www.imore.com/sites/imore.com/files/styles/large/public/field/image/2019/12/twitter-logo.jpg"
-            />
-          </a>
-        </div>
-      </div>
     </div>
   );
 };
