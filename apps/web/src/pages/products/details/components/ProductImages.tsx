2bccbff534388b5e51879e9d3eafacf3b7da5adb
Alvee <M.Alvee8141@gmail.com>
2024-04-30 02:53:48 +0600

    Update ProductImages.tsx

--- apps/web/src/pages/products/details/components/ProductImages.tsx
+++ apps/web/src/pages/products/details/components/ProductImages.tsx
@@ -19,14 +19,15 @@ const ProductImages: React.FC<ProductImagesPropTypes> = ({ Images }) => {
   );
   return (
     <div>
-      <img
-        alt="prodcut details"
-        className="mb-3"
-        sizes="(min-width: 1200px) 495px, (min-width: 990px) calc(45.0vw - 10rem), (min-width: 750px) calc((100vw - 11.5rem) / 2), calc(100vw / 1 - 4rem)"
-        src={selectedImage}
-        width={'500px'}
-        height={'500px'}
-      />
+      <div className="max-w-[500px] max-h-[500px] sm:w-[500px] sm:h-[500px] flex justify-center items-center">
+        <img
+          alt="prodcut details"
+          className="mb-3"
+          sizes="(min-width: 1200px) 495px, (min-width: 990px) calc(45.0vw - 10rem), (min-width: 750px) calc((100vw - 11.5rem) / 2), calc(100vw / 1 - 4rem)"
+          src={selectedImage}
+        />
+      </div>
+
       <Carousel className="w-full max-w-sm">
         <CarouselContent className="-ml-1">
           {Images.map((image, index) => (
